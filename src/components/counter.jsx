d818b8f9b3699f9ec478cef7cf6d534b71e3a6a1
Olha Yegorova <OYEGOROVA@pier1.com>
2019-11-21 17:05:35 +0200

    columns markup

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -35,32 +35,37 @@ class Counter extends React.Component {
     // console.log(this.props);
     console.log("Counter - Rendered");
     return (
-      <div>
-        <h4>Counter #{this.props.counter.id}</h4>
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button
-          onClick={() => {
-            this.props.onIncrement(this.props.counter);
-          }}
-          className="btn btn-secondary btn-sm"
-        >
-          +
-        </button>
-        <button
-          onClick={() => {
-            this.props.onDecrement(this.props.counter);
-          }}
-          className="btn btn-secondary btn-sm"
-          disabled={this.props.counter.value < 1}
-        >
-          -
-        </button>
-        <button
-          onClick={() => this.props.onDelete(this.props.counter.id)}
-          className="button btn btn-danger btn-sm m-2"
-        >
-          Delete
-        </button>
+      <div className="row">
+        {/* <h4>Counter #{this.props.counter.id}</h4> */}
+        <div className="col-1">
+          <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
+          <button
+            onClick={() => {
+              this.props.onIncrement(this.props.counter);
+            }}
+            className="btn btn-secondary btn-sm"
+          >
+            +
+          </button>
+          <button
+            onClick={() => {
+              this.props.onDecrement(this.props.counter);
+            }}
+            className="btn btn-secondary btn-sm m-2"
+            disabled={this.props.counter.value < 1}
+          >
+            -
+          </button>
+
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="button btn btn-danger btn-sm m-2"
+          >
+            Delete
+          </button>
+        </div>
       </div>
     );
   }
